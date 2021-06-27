71b02562b3e4f9c99ec1a8484567095eff1d7c91
danielPantalena <pantalenadaniel@gmail.com>
2021-06-27 18:51:29 -0300

    Create Model Post

--- wall/posts/models.py
+++ wall/posts/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.db.models import (
+    Model,
+    DateTimeField,
+    CharField,
+    TextField,
+    BooleanField,
+    ForeignKey,
+    CASCADE
+)
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(Model):
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+    title = CharField(max_length=255)
+    body = TextField()
+    deleted = BooleanField(default=False)
+    author = ForeignKey(User, on_delete=CASCADE)
+    
+
+    def __str__(self):
+        return self.title
