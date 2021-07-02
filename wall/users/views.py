06b70be16518b7b10ff1ede3f874edf3664f4ff8
danielPantalena <pantalenadaniel@gmail.com>
2021-07-02 11:57:47 -0300

    Create UsersList

--- wall/users/views.py
+++ wall/users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListAPIView,
+)
+from rest_framework.permissions import (
+    SAFE_METHODS,
+    IsAuthenticated,
+    BasePermission
+)
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+from .serializers import UserSerializer
+
+Users = get_user_model()
+
+'''
+class PostUserWritePermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_staff or request.user.is_superuser
+'''
+
+
+class UsersList(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = Users.objects.all()
