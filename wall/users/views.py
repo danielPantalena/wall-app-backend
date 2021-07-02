f8977d2228536cdbeae3c968174aad1363910fdc
danielPantalena <pantalenadaniel@gmail.com>
2021-07-02 17:34:54 -0300

    Create post method for UsersList

--- wall/users/views.py
+++ wall/users/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404, HttpResponse, HttpResponseBadRequest
 from rest_framework.generics import (
     ListCreateAPIView,
 )
@@ -8,8 +9,11 @@ from rest_framework.permissions import (
 )
 
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+import re
 
 from .serializers import UserSerializer
+
 
 '''
 class PostUserWritePermission(BasePermission):
@@ -23,3 +27,20 @@ class PostUserWritePermission(BasePermission):
 class UsersList(ListCreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def post(self, request):
+        new_user_email = request.data['email']
+        is_email_valid = re.match(r"^\w+@\w+\.[a-z]{2,}", new_user_email)
+        if not is_email_valid:
+            return HttpResponseBadRequest('A field with a valid email is necessary')
+
+        # send_mail(
+        #     'Congratulations! Your user - {} - was created at Wall App'.format(
+        #         request.data['username']),
+        #     'Hello {} {}, now you can login at Wall App using username and password that you created.'.format(
+        #         'hi', 'last_name'),
+        #     'Wall App',
+        #     [request.data['email']],
+        #     fail_silently=False,
+        # )
+        return HttpResponse(request.data['email'])
