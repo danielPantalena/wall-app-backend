5e626fed487f026e89a431eb616be1aec3193bc5
danielPantalena <pantalenadaniel@gmail.com>
2021-07-20 17:24:55 -0300

    Update tests

--- wall/users/tests.py
+++ wall/users/tests.py
@@ -28,21 +28,26 @@ class AccountTests(APITestCase):
                 'password': '4321', 'email': 'daniel@daniel.com'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        with self.assertRaises(IntegrityError):
-            self.client.post(url, data, format='json')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
 
-    def test_email_validation(self):
-        url = reverse('users-list')
-        data = {'username': 'daniel',
-                'password': '4321', 'email': 'daniel@'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        data['email'] = 'daniel@daniel.com'
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        data['email'] = 'daniel@daniel'
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    # def test_email_validation(self):
+    #     """
+    #     Ensure that email is valid.
+    #     """
+    #     url = reverse('users-list')
+    #     data = {'username': 'daniel',
+    #             'password': '4321', 'email': 'daniel@'}
+    #     response = self.client.post(url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    #     data['email'] = 'daniel@daniel.com'
+    #     response = self.client.post(url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     data['email'] = 'daniel@daniel'
+    #     response = self.client.post(url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_email_sender(self):
         """
